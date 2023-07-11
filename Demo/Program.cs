14c30947e880356b91b892c38c9cc2109aee1639
kalshettigayatri <gayatrikalshetti3@gmail.com>
2023-07-11 16:42:48 +0530

    WElcome to EmployeeWagsUC5

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,10 +1,15 @@
 ﻿namespace DEMO
 {
-    internal class Program
+     internal class Program
     {
-        static void Main(string[] args)
+         public static void AdditionOfTwoNUmbers()
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("PLease Enter the first number");
+            int firstNumb = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("please enter the second number");
+            int secondNumber=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("sum:"+(firstNumb + secondNumber));
         }
+        
     }
 }
